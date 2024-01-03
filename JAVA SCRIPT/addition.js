724690b89ed0528508c5a5a2663597cc6015c9e5
shrihari thakare <143932019+ShrihariThakare@users.noreply.github.com>
2024-01-03 18:01:09 +0530

    Add files via upload

---
+++ JAVA SCRIPT/addition.js
@@ -0,0 +1,9 @@
+// Define two numbers
+let number1 = 5;
+let number2 = 10;
+
+// Perform addition
+let sum = number1 + number2;
+
+// Display the result
+console.log("The sum of", number1, "and", number2, "is:", sum);
