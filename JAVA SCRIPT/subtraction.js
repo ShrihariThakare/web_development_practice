89d473b42b7369a97a5489870595a94b7e487cab
shrihari thakare <143932019+ShrihariThakare@users.noreply.github.com>
2024-01-04 21:53:06 +0530

    Add files via upload

---
+++ JAVA SCRIPT/subtraction.js
@@ -0,0 +1,9 @@
+// Define two numbers
+const number1 = 10;
+const number2 = 5;
+
+// Perform subtraction
+const result = number1 - number2;
+
+// Display the result
+console.log(`The result of ${number1} - ${number2} is: ${result}`);
