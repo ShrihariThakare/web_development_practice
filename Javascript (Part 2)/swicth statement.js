810d6ee8bfa4ded5e148806b489da3260ce93c87
shrihari thakare <143932019+ShrihariThakare@users.noreply.github.com>
2024-01-07 21:10:26 +0530

    Add files via upload

---
+++ Javascript (Part 2)/swicth statement.js
@@ -0,0 +1,15 @@
+let color = "green";
+
+switch(color){
+    case "red":
+        console.log("stop");
+        break;
+        case "yellow":
+            console.log("slow down");
+            break;
+            case "green":
+                console.log("go");
+                break;
+                default:
+                    console.log("broken light");
+}
