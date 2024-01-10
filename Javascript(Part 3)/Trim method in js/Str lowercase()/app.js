2e7a95a850ba73aea5343a9ee45a9e2381772718
shrihari thakare <143932019+ShrihariThakare@users.noreply.github.com>
2024-01-10 18:56:44 +0530

    Add files via upload

---
+++ Javascript(Part 3)/Trim method in js/Str lowercase()/app.js
@@ -0,0 +1,4 @@
+let msg = "STRING ARE IMMUTABLE";
+console.log(msg.toLowerCase());
+//please check the console "STRING ARE IMMUTABLE" print as a "string are immutable".
+//This is using string method "str.Lowercase()".
