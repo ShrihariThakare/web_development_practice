02312edc22bbdf21faa1d7ef4d973d04fd11abd4
shrihari thakare <143932019+ShrihariThakare@users.noreply.github.com>
2024-01-24 21:37:20 +0530

    Add files via upload

---
+++ javascript (part 7)/this key word in js/app.js
@@ -0,0 +1,11 @@
+const student={
+    name:"shrihari",
+    age:"20",
+    eng:"95",
+    math:"93",
+    phy:"85",
+    getAvg(){
+        letAvg=(this.eng + this.math + this.phy) /3;
+        console.log(avg);
+    }
+}
